0f2739cccd9ff52442f1b8347be7361a08045e70
Owen Smith <owen@omsmith.ca>
2013-11-21 14:03:48 -0500

    initial implementation

---
+++ index.js
@@ -0,0 +1,26 @@
+var ngAnnotate = require('ng-annotate'),
+	through = require('through');
+
+module.exports = function () {
+	var data = '';
+
+	return through(write, end);
+
+	function write (buf) {
+		data += buf;
+	}
+
+	function end () {
+		var annotateResult = ngAnnotate(data, {
+			add: true,
+			remove: true
+		});
+		if (annotateResult.errors) {
+			annotateResult.errors.forEach(function (error) {
+				this.emit(error);
+			});
+		}
+		this.queue(annotateResult.src);
+		this.queue(null);
+	}
+};
