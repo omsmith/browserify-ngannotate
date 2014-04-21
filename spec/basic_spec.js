5bb414e5c3fc8d69cde7c664c3d71104e708a059
Owen Smith <owen@omsmith.ca>
2014-04-21 18:58:12 -0400

    (test) add test for passing in ng-annotate options

--- spec/basic_spec.js
+++ spec/basic_spec.js
@@ -7,13 +7,35 @@ var ngannotate = require('../'),
 	through = require('through');
 
 describe('basic directive', function () {
-	it('should be annotated', function (done) {
+	it('should be annotated by default', function (done) {
 		var data = '',
-			expected = 'angular.module(\'basic\', []).directive(\'basic\', ["someCtrl", function (someCtrl) {}]);';
+			expected = fs.readFileSync(require.resolve('./files/basic_annotated.js'), 'utf-8');
 
 		fs
 			.createReadStream(require.resolve('./files/basic.js'))
 			.pipe(ngannotate())
+			.pipe(through(function (buf) {
+				data += buf;
+			}, function () {
+				var err;
+				if (data !== expected) {
+					err = new Error('expected "' + data + '" to be "' + expected + '"');
+				}
+
+				done(err);
+			}));
+	});
+
+	it('should have annotations removed', function (done) {
+		var data = '',
+			expected = fs.readFileSync(require.resolve('./files/basic.js'), 'utf-8');
+
+		fs
+			.createReadStream(require.resolve('./files/basic_annotated.js'))
+			.pipe(ngannotate(require.resolve('./files/basic_annotated.js'), {
+				add: false,
+				remove: true
+			}))
 			.pipe(through(function (buf) {
 				data += buf;
 			}, function () {
