83f443839afc898b01c53b75652eaaf062164e24
Owen Smith <owen@omsmith.ca>
2014-05-26 01:26:46 -0400

    actually use browserify in tests

--- spec/basic_spec.js
+++ spec/basic_spec.js
@@ -4,47 +4,49 @@
 
 var ngannotate = require('../'),
 	fs = require('fs'),
-	through = require('through');
+	through = require('through'),
+	browserify = require('browserify');
 
 describe('basic directive', function () {
 	it('should be annotated by default', function (done) {
 		var data = '',
-			expected = fs.readFileSync(require.resolve('./files/basic_annotated.js'), 'utf-8');
+			expected = fs.readFileSync(require.resolve('./files/basic_expected.js'), 'utf-8');
 
-		fs
-			.createReadStream(require.resolve('./files/basic.js'))
-			.pipe(ngannotate())
-			.pipe(through(function (buf) {
-				data += buf;
-			}, function () {
-				var err;
-				if (data !== expected) {
-					err = new Error('expected "' + data + '" to be "' + expected + '"');
-				}
+		browserify({
+			entries: require.resolve('./files/basic.js')
+		})
+		.transform(ngannotate)
+		.bundle()
+		.pipe(through(function (buf) {
+			data += buf;
+		}, function () {
+			var err;
+			if (data !== expected) {
+				err = new Error('expected "' + data + '" to be "' + expected + '"');
+			}
 
-				done(err);
-			}));
+			done(err);
+		}));
 	});
 
 	it('should have annotations removed', function (done) {
 		var data = '',
-			expected = fs.readFileSync(require.resolve('./files/basic.js'), 'utf-8');
+			expected = fs.readFileSync(require.resolve('./files/basic_annotated_expected.js'), 'utf-8');
 
-		fs
-			.createReadStream(require.resolve('./files/basic_annotated.js'))
-			.pipe(ngannotate(require.resolve('./files/basic_annotated.js'), {
-				add: false,
-				remove: true
-			}))
-			.pipe(through(function (buf) {
-				data += buf;
-			}, function () {
-				var err;
-				if (data !== expected) {
-					err = new Error('expected "' + data + '" to be "' + expected + '"');
-				}
+		browserify({
+			entries: require.resolve('./files/basic_annotated.js')
+		})
+		.transform({ add: false, remove: true }, ngannotate)
+		.bundle()
+		.pipe(through(function (buf) {
+			data += buf;
+		}, function () {
+			var err;
+			if (data !== expected) {
+				err = new Error('expected "' + data + '" to be "' + expected + '"');
+			}
 
-				done(err);
-			}));
+			done(err);
+		}));
 	});
 });
